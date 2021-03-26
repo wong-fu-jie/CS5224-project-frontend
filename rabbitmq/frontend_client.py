0e374da885107e52a6ebc9cdaeb78a650377ef61
limchunhan97 <79507543+limchunhan97@users.noreply.github.com>
2021-03-26 15:16:38 +0800

    Add files via upload

---
+++ rabbitmq/frontend_client.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import pika
+import uuid
+
+class FrontEnd(object):
+
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='frontend_optimisation',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return int(self.response)
+
+
+frontend_client = FrontEnd()
+
+print(" [x] Requesting fib(2)")
+response = frontend_client.call(2)
+print(" [.] Got %r" % response)
---
+++ rabbitmq/optimisation_server.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import pika
+import uuid
+
+class Optimisation(object):
+
+    def __init__(self):
+
+        self.connection1 = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+
+        self.connection2 = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+
+		self.connection3 = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))	
+
+        self.channel1 = self.connection1.channel()
+
+        self.channel2 = self.connection2.channel()
+
+		self.channel3 = self.connection3.channel()
+
+        result1 = self.channel1.queue_declare(queue = 'frontend_optimisation')
+        result2 = self.channel2.queue_declare(queue = 'optimisation_onemap')
+		result3 = self.channel3.queue_declare(queue = 'recommendation_optimisation')
+
+        self.callback_queue1 = result1.method.queue
+        self.callback_queue2 = result2.method.queue
+		self.callback_queue3 = result3.method.queue
+
+        self.channel1.basic_qos(prefetch_count=1)
+
+        self.channel1.basic_consume(queue='frontend_optimisation', on_message_callback=self.on_request)
+		self.channel3.basic_consume(queue='recommendation_optimisation', on_message_callback=self.on_request)
+
+        self.channel2.basic_consume(
+            queue = self.callback_queue2,
+            on_message_callback = self.on_response,
+            auto_ack = True
+        )
+
+    def fib(self, n):
+
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.fib(n - 1) + self.fib(n - 2)
+
+    def on_request(self, ch, method, props, body):
+        n = int(body)
+
+        print(" [.] Got request for fib(%s)" % n)
+        # response = self.fib(n)
+
+        response = self.call(n)
+
+        ch.basic_publish(exchange='',
+                        routing_key=props.reply_to,
+                        properties=pika.BasicProperties(correlation_id = \
+                                                            props.correlation_id),
+                        body=str(response))
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def call(self, n):
+
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+
+        print(" [.] Requesting for fib(%s) to OneMap API" % n)
+        self.channel2.basic_publish(
+            exchange='',
+            routing_key='optimisation_onemap',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue2,
+                correlation_id=self.corr_id,
+            ),
+            body=str(n))
+        while self.response is None:
+            self.connection2.process_data_events()
+
+        print(' [.] Response received! Relaying to frontend now...')
+
+        return int(self.response)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+optimisation = Optimisation()
+print(" [x] Awaiting RPC requests")
+optimisation.channel1.start_consuming()
