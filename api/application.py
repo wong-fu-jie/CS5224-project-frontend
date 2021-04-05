30fd5944fdd90a2fc1ac4d3ac0cf8a2754532ed6
limchunhan97 <79507543+limchunhan97@users.noreply.github.com>
2021-04-05 14:43:12 +0800

    Edited for Amazon MQ

--- api/application.py
+++ api/application.py
@@ -3,8 +3,50 @@ from flask import Flask, jsonify, request, json
 
 from sample_itinerary import *
 
+import pika
+import uuid
+
 app = Flask(__name__)
 
+class FrontEnd(object):
+
+    def __init__(self):
+
+        parameters = pika.URLParameters('amqps://Administrator:administrator123@b-8894e7fb-ac9c-4657-8912-473a0bf03efa.mq.us-east-1.amazonaws.com:5671/%2f')
+        self.connection = pika.BlockingConnection(parameters)
+
+        # self.connection = pika.BlockingConnection(
+        #     pika.ConnectionParameters(host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, request_data):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='frontend_recommendation',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=list(request_data))
+        while self.response is None:
+            self.connection.process_data_events()
+        return list(self.response)
 
 @app.route('/')
 def home():
@@ -19,6 +61,10 @@ def get_current_time():
 @app.route('/api/recommend', methods=['POST'])
 def submit_recommend():
     request_data = json.loads(request.data)
+
+    frontend_client = FrontEnd()
+    
+    response = frontend_client.call(request_data)
 
     # Publish recommend message
 
